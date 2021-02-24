3db48a6f08451aa4f3c6deed3b3f9e0909abb8fc
timurturbil <timurturbil@hotmail.com>
2021-02-24 11:44:23 +0300

    changes are done

--- main/app.js
+++ main/app.js
@@ -7,3 +7,31 @@ function myfunction() {
         document.getElementById("theTextarea2").innerHTML = data.quotes[0].author;
     });   
    }  
+ /*   'URL','heyyy' */
+Share = {
+    twitter: function() {
+    url = 'http://twitter.com/share?';
+    url += 'text=' + encodeURIComponent(document.getElementById("theTextarea").innerHTML + "  " + "Author: " + document.getElementById("theTextarea2").innerHTML);
+    url += '&url=' + encodeURIComponent(' ');
+    Share.popup(url);
+    },
+    popup: function(url) {
+    window.open(url,'','toolbar=0,status=0,width=626, height=436');
+    }
+    };
+    
+
+/* 
+    <a onclick="Share.twitter('URL','TITLE')"> Share the quote in twitter</a>
+    <a href="https://twitter.com/intent/tweet?original_referer=
+    http%3A%2F%2Ffiddle.jshell.net%2F_display%2F&text=TITLE&url
+    =URL" target="_blank" onclick="return Share.me(this)">Share</a> */
+
+
+
+/*     <a href="https://twitter.com/intent/tweet?original_referer=
+    http%3A%2F%2Ffiddle.jshell.net%2F_display%2F&text=TITLE&url
+    =URL" target="_blank" onclick="return Share.me(this)">Share</a> */
+
+
+    
